716e26bceebd5cc2fd20e084b3a81fc654d930fc
Yuvraj9928220015 <yuvrajsharma190105@gmail.com>
2026-02-21 14:00:45 +0530

    New CORS update

--- Backend/index.js
+++ Backend/index.js
@@ -23,8 +23,6 @@ console.log('Database Type:', dbType);
 console.log('MongoDB URL:', MONGO_URL.substring(0, 40) + '...');
 
 // Middleware
-const cors = require("cors");
-
 app.use(cors({
   origin: "*",
   methods: ["GET","POST","PUT","DELETE"],
